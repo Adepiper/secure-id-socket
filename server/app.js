fdd1b0f806969593fa08771ea85836f119a683c5
Oluwatobiloba Adebayo <hardebaryorone@gmail.com>
2023-09-12 18:24:35 +0100

    updated socket server

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const socket = require("socket.io");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(bodyParser);
+let x = true;
+
+const server = app.listen(3000, () => {
+  console.log("Started in 3000");
+});
+
+const io = socket(server);
+
+io.sockets.on("connection", (socket) => {
+  sendData(socket);
+});
+
+function sendData(socket) {
+  if (x) {
+    socket.emit(
+      "dataset1",
+      Array.from({ length: 8 }, () => Math.floor(Math.random() * 590) + 10)
+    );
+    x = !x;
+  } else {
+    socket.emit(
+      "dataset2",
+      Array.from({ length: 8 }, () => Math.floor(Math.random() * 590) + 10)
+    );
+    x = !x;
+  }
+  console.log(`data is ${x}`);
+  setTimeout(() => {
+    sendData(socket);
+  }, 3000);
+}
